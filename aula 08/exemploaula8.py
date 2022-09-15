66e0c88fa07192cf8e9a6d65cdda73eff0fd47e9
Luiz Carlos Santana <112254950+luizcarlossantana@users.noreply.github.com>
2022-09-15 20:42:44 -0300

    iiii

--- aula 08/exemploaula8.py
+++ aula 08/exemploaula8.py
@@ -2,3 +2,11 @@ import math
 x = int(input('digite um número:'))
 raiz = math.sqrt(x)
 print(f'a raiz quadrada de {x} é {raiz:.1f}')
+
+
+# Outra forma de fazer 
+
+from math import sqrt,ceil
+x = int(input('digite um número:'))
+raiz = sqrt(x)
+print(f'a raiz quadrada de {x} é {ceil(raiz):.2f}')
